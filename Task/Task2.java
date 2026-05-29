93d2fe9d896bdc59677da8f3d369f642411bb538
zyxer30 <zyxer30@gmail.com>
2026-05-29 14:21:07 +0000

    Task 2

---
+++ Task/Task2.java
@@ -0,0 +1,15 @@
+public class Task2 {
+public static void main(String[] args) {
+    boolean bol = true;
+    byte byt = 0;
+    short st = 3;
+    int n = 110;
+    String s = "rld";
+    char H = 72;
+    char w = 'w';
+    float fl = 2.0f;
+    String Output = String.valueOf(H)+st+n+" "+w+byt+s+" "+fl+" "+bol;
+    
+    System.out.println(Output);
+}
+}
