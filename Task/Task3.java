b9e0f0d3c543c94c5f4b758a036d393ba6c000fc
zyxer30 <zyxer30@gmail.com>
2026-06-02 08:17:35 +0000

    Large Number Task

---
+++ Task/Task3.java
@@ -0,0 +1,17 @@
+public class Task3 {
+    public static void main(String[] args) {
+        String a = new String("Wow");
+        String b = "Wow";
+        String c = a;
+        String d = c;
+
+        boolean b1 = a.toString().equals(b);
+        boolean b2 = d.equals(b);
+        boolean b3 = c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success");
+            
+        }
+    }
+}
