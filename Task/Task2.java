137b4985668e98a8292df0154b254cb4161b4876
zyxer30 <zyxer30@gmail.com>
2026-06-03 06:09:35 +0000

    Task to submit

--- Task/Task2.java
+++ Task/Task2.java
@@ -3,12 +3,12 @@ public static void main(String[] args) {
     boolean bol = true;
     byte byt = 0;
     short st = 3;
-    int n = 110;
+    int num = 110;
     String s = "rld";
     char H = 72;
     char w = 'w';
     float fl = 2.0f;
-    String Output = new String(new char[]{H})+st+n+" "+w+byt+s+" "+fl+" "+bol;
+    String Output = new String(new char[]{H})+st+num+" "+w+byt+s+" "+fl+" "+bol;
     
     System.out.println(Output);
 }
