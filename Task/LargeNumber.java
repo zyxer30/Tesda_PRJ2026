6ddd09bdd95a1af643b663c5f4f4bd857f4214e8
zyxer30 <zyxer30@gmail.com>
2026-06-03 06:30:09 +0000

    submition

---
+++ Task/LargeNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class LargeNumber {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+
+
+        System.out.println("Enter 1st Number?");
+        int a = num.nextInt();
+
+        System.out.println("Enter 2nd Number?");
+        int b = num.nextInt();
+
+        System.out.println("Enter 3rd Number?");
+        int c = num.nextInt();
+
+        if (a>b && a>c) {
+            System.out.println("Larger number is " + a);
+        } else if (b>a && b>c) {
+            System.out.println("Larger number is " + b);
+        } else {
+            System.out.println("Larger number is " + c);
+        }
+
+        num.close();
+
+
+        
+
+    }
+
+}
