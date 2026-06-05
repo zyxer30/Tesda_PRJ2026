80bfb6d67090afa978cb586cb57d44825e3652f7
zyxer30 <zyxer30@gmail.com>
2026-06-05 07:33:23 +0000

    Task Submition

--- Task/LargeNumber.java
+++ Task/LargeNumber.java
@@ -1,4 +1,19 @@
 import java.util.Scanner;
+/***
+ * LargeNumber.java
+ * 
+ * Class name LargeNumber.java
+ * Asks the user to input 3 numbers and prints the largest number.
+ * 
+ *  Enter 1st Number?
+ *  10
+ *  Enter 2nd Number?
+ *  20
+ *  Enter 3rd Number?
+ *  30
+ *  Larger number is 30
+ */ 
+
 
 public class LargeNumber {
     public static void main(String[] args) {
