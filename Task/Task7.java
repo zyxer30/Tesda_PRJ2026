709bddd29d8498dd2a31d657d97f695f659744b2
zyxer30 <zyxer30@gmail.com>
2026-06-17 05:49:11 +0000

    Task 7 Submit

---
+++ Task/Task7.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/***
+ * Task 7
+ * 
+ * Create a program where user inputs two numbers and performs 4 basic arithmetic operations with return types and print the results.
+ */
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = num.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = num.nextInt();
+
+        System.out.println("Addition: " + add(num1, num2));
+        System.out.println("Subtraction: " + subtract(num1, num2));
+        System.out.println("Multiplication: " + multiply(num1, num2));
+        System.out.println("Division: " + divide(num1, num2));
+
+        System.out.println();
+        num.close();
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        return (double) a / b;
+    }
+}
